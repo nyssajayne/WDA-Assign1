093dd3b9b0a539debcd5f1826cd7037ce547a67e
Ubuntu <ubuntu@ip-172-31-16-35.ap-southeast-2.compute.internal>
2013-08-20 06:11:59 +0000

    updated db connection to use pdo

--- connect.php
+++ connect.php
@@ -5,26 +5,16 @@
 	error_reporting(E_ALL | E_STRICT);
 
 	//Require database credentials
-	require('db.php');
+	require_once('db.php');
 
-	//Establish connection to MySQL
-	$conn = mysql_connect(DB_HOST, DB_USER, DB_PW);
-
-	if(!$conn)
+	//Establish connection
+	try
 	{
-		echo '<p>Could not connect to MySQL on ' . DB_HOST . '<br />';
-		echo mysql_error();
-		echo '</p>';
-		exit;
+		$dsn = DB_ENGINE .':host='. DB_HOST .';dbname='. DB_NAME;
+		$db = new PDO($dsn, DB_USER, DB_PW);
 	}
-	
-	//Establish connection to database
-	$dbconn = mysql_select_db(DB_NAME, $conn);
-
-	if(!$dbconn)
+	catch(PDOException $e)
 	{
-		echo '<p>Could not connect to database ' .DB_NAME . '<br />';
-		echo mysql_error();
-		echo '</p>';
-		exit;
+		echo $e->getMessage();
 	}
+?>
